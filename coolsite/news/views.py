e19576c61f3ba8e52771494af10bcc0ff107ab81
dmytro.lynda <dmytro.lynda@gmail.com>
2021-04-22 14:32:10 +0200

    Added categories

--- coolsite/news/views.py
+++ coolsite/news/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News, Category
+
 
 def index(request):
-    print(request)
-    return HttpResponse('Hello world!')
+    news = News.objects.all()
+    categories = Category.objects.all()
+    context = {
+        'news': news,
+        'title': 'news list',
+        'categories': categories,
+    }
+    return render(request, 'news/index.html', context)
 
-def test(request):
-    return HttpResponse('<h1>Test test test</h1>')
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news, 'categories': categories, 'category': category})
